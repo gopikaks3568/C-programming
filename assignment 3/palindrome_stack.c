01c3dd8e2d9add9c411959a6716cb03d6680ee7d
gopikaks <gopikaks3568@gmail.com>
2022-12-04 21:07:42 +0530

    rewrote the program

--- assignment 3/palindrome_stack.c
+++ assignment 3/palindrome_stack.c
@@ -1,73 +1,53 @@
 // C implementation of the approach
-#include <malloc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+char stack[50];
+int top=-1;
 
-char* stack;
-int top = -1;
 
-// push function
-void push(char ele)
+void push(char arg)
 {
-	stack[++top] = ele;
+	top=top+1;
+	stack[top]=arg;
 }
 
-// pop function
 char pop()
 {
-	return stack[top--];
+	char c;
+	c=stack[top];
+	top=top+1;
+	return c;
 }
-
-// Function that returns 1
-// if str is a palindrome
-int isPalindrome(char str[])
-{
-	int length = strlen(str);
-
-	// Allocating the memory for the stack
-	stack = (char*)malloc(length * sizeof(char));
-
-	// Finding the mid
-	int i, mid = length / 2;
-
-	for (i = 0; i < mid; i++) {
-		push(str[i]);
-	}
-
-	// Checking if the length of the string
-	// is odd, if odd then neglect the
-	// middle character
-	if (length % 2 != 0) {
-		i++;
-	}
-
-	// While not the end of the string
-	while (str[i] != '\0') {
-		char ele = pop();
-
-		// If the characters differ then the
-		// given string is not a palindrome
-		if (ele != str[i])
-			return 0;
-		i++;
-	}
-
-	return 1;
-}
-
-// Driver code
 int main()
 {
-	char str[100];
-    printf("Enter the   ")
-
-	if (isPalindrome(str)) {
-		printf("Yes");
+	char string[30];
+	int i,count=0,len;
+	printf("Enetr the string:\n");
+	scanf("%s",&string);
+	len=strlen(string);
+	for ( i = 0; i < len; i++)
+	{
+		push(string[i]);
 	}
-	else {
-		printf("No");
-	}
+	for ( i = 0; i < len; i++)
+	{
+		if (string[i]==pop()){
+			count++;
 
-	return 0;
+		}
+	}
+	if (len==count)
+	{
+		printf("palindrome");
+	}
+	else
+	{
+		printf("not palidrome");
+	}
+	
+	
+	
+	
+
+
 }
