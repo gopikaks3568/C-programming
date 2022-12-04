35c349cae385d5a118740c57448b2aaeab5301e1
gopikaks <gopikaks3568@gmail.com>
2022-12-04 21:12:10 +0530

    rewrote it again

--- assignment 3/palindrome_stack.c
+++ assignment 3/palindrome_stack.c
@@ -1,53 +1,49 @@
-// C implementation of the approach
+//program to check given string is palidrome or not
 #include<stdio.h>
 #include<string.h>
 char stack[50];
 int top=-1;
 
-
 void push(char arg)
 {
-	top=top+1;
-	stack[top]=arg;
+    top=top+1;
+    stack[top]=arg;
+}
+char pop(){
+    char c;
+    c=stack[top];
+    top=top-1;
+    return c;
 }
 
-char pop()
-{
-	char c;
-	c=stack[top];
-	top=top+1;
-	return c;
-}
+
 int main()
 {
-	char string[30];
-	int i,count=0,len;
-	printf("Enetr the string:\n");
-	scanf("%s",&string);
-	len=strlen(string);
-	for ( i = 0; i < len; i++)
-	{
-		push(string[i]);
-	}
-	for ( i = 0; i < len; i++)
-	{
-		if (string[i]==pop()){
-			count++;
+    char string[30];
+    int i,count=0,len;
+    printf("Enter the String\n");
+    scanf("%s",string);
+    len=strlen(string);
+    for ( i = 0; i < len; i++)
+    {
+        push(string[i]);
+    }
+     for ( i = 0; i < len; i++)
+     {
+        if (string[i]==pop())
+        {
+            count=count+1;
+        }
 
-		}
-	}
-	if (len==count)
-	{
-		printf("palindrome");
-	}
-	else
-	{
-		printf("not palidrome");
-	}
-	
-	
-	
-	
+     }
+     if (len==count){
+        printf("Given String is palindrome\n");
+     }
+     else
+     {
+        printf("given String is not palindrome");
+     }
+
 
 
 }
