dfc9e68d6b000b86782bb61cd707770a5e9fe24f
gopikaks <gopikaks3568@gmail.com>
2022-12-25 18:53:36 +0530

    Pq using array uploaded

--- priortyqueue_array.c
+++ priortyqueue_array.c
@@ -1,38 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
 #define MAX_SIZE 100
-typedef struct 
-{
-    int priority;
-    int value;
+
+typedef struct {
+  int data;
+  int priority;
 } Element;
 
-typedef struct 
-{
-    Element elements[MAX_SIZE];
-    int size;
-}priorityQueue;
+typedef struct {
+  Element elements[MAX_SIZE];
+  int size;
+} PriorityQueue;
 
-void intit(priorityQueue *queue){
-    queue->size=0;
+void push(PriorityQueue *queue, int data, int priority) {
+  if (queue->size >= MAX_SIZE) {
+    // Queue is full, cannot add more elements
+    return;
+  }
 
-}
-
-int is_empty(priorityQueue *queue){
-    return queue->size==0;
-}
-
-void push(priorityQueue *queue,int value ,int priority){
-    if (queue->size==MAX_SIZE){
-        printf("Error: queue is full \n");
-        return;
+  Element element = {data, priority};
+  int i;
+  for (i = queue->size - 1; i >= 0; i--) {
+    if (queue->elements[i].priority <= priority) {
+      break;
     }
+    queue->elements[i + 1] = queue->elements[i];
+  }
+  queue->elements[i + 1] = element;
+  queue->size++;
 }
 
-Element new_element;
-new_element.priority=priority;
-new_element.value=value;
-int i;
-for(i=queue->size;i>0&& queue->elements[i-1].priority <priority;i--){
-    
+int pop(PriorityQueue *queue) {
+  if (queue->size == 0) {
+    // Queue is empty, cannot pop an element
+    return -1;
+  }
+
+  int data = queue->elements[queue->size - 1].data;
+  queue->size--;
+  return data;
+}
+
+int main() {
+  PriorityQueue queue;
+  queue.size = 0;
+
+  push(&queue, 1, 2);
+  push(&queue, 2, 1);
+  push(&queue, 3, 3);
+  push(&queue, 4, 0);
+
+  printf("%d\n", pop(&queue));  // 4
+  printf("%d\n", pop(&queue));  // 2
+  printf("%d\n", pop(&queue));  // 1
+  printf("%d\n", pop(&queue));  // 3
 }
