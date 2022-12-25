da9d780c326dbcab6ce26863591cec4b7fc06076
gopikaks <gopikaks3568@gmail.com>
2022-12-25 19:04:40 +0530

    changed made

--- priortyqueue_array.c
+++ priortyqueue_array.c
@@ -1,56 +1,65 @@
-#include<stdio.h>
+#include <stdio.h>
 #define MAX_SIZE 100
 
-typedef struct {
-  int data;
-  int priority;
+typedef struct
+{
+    int data;
+    int priority;
 } Element;
 
-typedef struct {
-  Element elements[MAX_SIZE];
-  int size;
+typedef struct
+{
+    Element elements[MAX_SIZE];
+    int size;
 } PriorityQueue;
 
-void push(PriorityQueue *queue, int data, int priority) {
-  if (queue->size >= MAX_SIZE) {
-    // Queue is full, cannot add more elements
-    return;
-  }
-
-  Element element = {data, priority};
-  int i;
-  for (i = queue->size - 1; i >= 0; i--) {
-    if (queue->elements[i].priority <= priority) {
-      break;
+void push(PriorityQueue *queue, int data, int priority)
+{
+    if (queue->size >= MAX_SIZE)
+    {
+        // Queue is full, cannot add more elements
+        return;
     }
-    queue->elements[i + 1] = queue->elements[i];
-  }
-  queue->elements[i + 1] = element;
-  queue->size++;
+
+    Element element = {data, priority};
+    int i;
+    for (i = queue->size - 1; i >= 0; i--)
+    {
+        if (queue->elements[i].priority <= priority)
+        {
+            break;
+        }
+        queue->elements[i + 1] = queue->elements[i];
+    }
+    queue->elements[i + 1] = element;
+    queue->size++;
 }
 
-int pop(PriorityQueue *queue) {
-  if (queue->size == 0) {
-    // Queue is empty, cannot pop an element
-    return -1;
-  }
+int pop(PriorityQueue *queue)
+{
+    if (queue->size == 0)
+    {
+        // Queue is empty, cannot pop an element
+        return -1;
+    }
 
-  int data = queue->elements[queue->size - 1].data;
-  queue->size--;
-  return data;
+    int data = queue->elements[queue->size - 1].data;
+    queue->size--;
+    return data;
 }
 
-int main() {
-  PriorityQueue queue;
-  queue.size = 0;
+int main()
+{
+    PriorityQueue queue;
+    queue.size = 0;
 
-  push(&queue, 1, 2);
-  push(&queue, 2, 1);
-  push(&queue, 3, 3);
-  push(&queue, 4, 0);
+    push(&queue, 1, 2);
+    push(&queue, 2, 1);
+    push(&queue, 3, 3);
+    push(&queue, 4, 0);
 
-  printf("%d\n", pop(&queue));  // 4
-  printf("%d\n", pop(&queue));  // 2
-  printf("%d\n", pop(&queue));  // 1
-  printf("%d\n", pop(&queue));  // 3
+    printf("%d\n", pop(&queue)); // 4
+    printf("%d\n", pop(&queue)); // 2
+    printf("%d\n", pop(&queue)); // 1
+    printf("%d\n", pop(&queue)); // 3
 }
