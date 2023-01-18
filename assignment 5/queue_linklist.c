3c6de478abbd34691e766e78e88395d42a7c0f5f
gopikaks <gopikaks3568@gmail.com>
2023-01-18 10:37:16 +0530

    Donut released

--- assignment 5/queue_linklist.c
+++ assignment 5/queue_linklist.c
@@ -1,107 +1,109 @@
-//The program for queue using link list
-//degubbed and wokring 
-#include<stdio.h>   
-#include<stdlib.h>  
-struct node   
-{  
-    int data;   
-    struct node *next;  
-};  
-struct node *front;  
-struct node *rear;   
-void insert();  
-void delete();  
-void display();  
-void main ()  
-{  
-    int choice;   
-    while(choice != 4)   
-    {     
-        printf("\n*************************Main Menu*****************************\n");  
-        printf("\n=================================================================\n");  
-        printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");  
-        printf("\nEnter your choice:");  
-        scanf("%d",& choice);  
-        switch(choice)  
-        {  
-            case 1:  
-            insert();  
-            break;  
-            case 2:  
-            delete();  
-            break;  
-            case 3:  
-            display();  
-            break;  
-            case 4:
+// The program for queue using link list
+// degubbed and wokring
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *front;
+struct node *rear;
+void insert();
+void delete();
+void display();
+void main()
+{
+    int choice;
+    while (choice != 4)
+    {
+        printf("\n*************************Main Menu*****************************\n");
+        printf("\n=================================================================\n");
+        printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+        printf("\nEnter your choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert();
+            break;
+        case 2:
+            delete ();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
             printf("Chose to exit");
-            exit(0);  
-            break;  
-            default:   
-            printf("\nInvalid entery!!!\n");  
-        }  
-    }  
-}  
-void insert()  
-{  
-    struct node *ptr;  
-    int item;   
-      
-    ptr = (struct node *) malloc (sizeof(struct node));  
-    if(ptr == NULL)  
-    {  
-        printf("\nOVERFLOW\n");  
-        return;  
-    }  
-    else  
-    {   
-        printf("\nEnter value:");  
-        scanf("%d",&item);  
-        ptr -> data = item;  
-        if(front == NULL)  
-        {  
-            front = ptr;  
-            rear = ptr;   
-            front -> next = NULL;  
-            rear -> next = NULL;  
-        }  
-        else   
-        {  
-            rear -> next = ptr;  
-            rear = ptr;  
-            rear->next = NULL;  
-        }  
-    }  
-}     
-void delete ()  
-{  
-    struct node *ptr;  
-    if(front == NULL)  
-    {  
-        printf("\nUNDERFLOW\n");  
-        return;  
-    }  
-    else   
-    {  printf("The element is popped\n");
-        ptr = front;  
-        front = front -> next;  
-        free(ptr);  
-    }  
-}  
-void display()  
-{  
-    struct node *ptr;  
-    ptr = front;      
-    if(front == NULL)  
-    {  
-        printf("\nEmpty queue\n");  
-    }  
-    else  
-    {   printf("\nprinting values .....\n");  
-        while(ptr != NULL)   
-        {  
-            printf("\n%d\n",ptr -> data);  
-            ptr = ptr -> next;  
-        }  
-    }  
-}  
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid entery!!!\n");
+        }
+    }
+}
+void insert()
+{
+    struct node *ptr;
+    int item;
+
+    ptr = (struct node *)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("\nOVERFLOW\n");
+        return;
+    }
+    else
+    {
+        printf("\nEnter value:");
+        scanf("%d", &item);
+        ptr->data = item;
+        if (front == NULL)
+        {
+            front = ptr;
+            rear = ptr;
+            front->next = NULL;
+            rear->next = NULL;
+        }
+        else
+        {
+            rear->next = ptr;
+            rear = ptr;
+            rear->next = NULL;
+        }
+    }
+}
+void delete()
+{
+    struct node *ptr;
+    if (front == NULL)
+    {
+        printf("\nUNDERFLOW\n");
+        return;
+    }
+    else
+    {
+        printf("The element is popped\n");
+        ptr = front;
+        front = front->next;
+        free(ptr);
+    }
+}
+void display()
+{
+    struct node *ptr;
+    ptr = front;
+    if (front == NULL)
+    {
+        printf("\nEmpty queue\n");
+    }
+    else
+    {
+        printf("\nprinting values .....\n");
+        while (ptr != NULL)
+        {
+            printf("\n%d\n", ptr->data);
+            ptr = ptr->next;
+        }
+    }
+}
